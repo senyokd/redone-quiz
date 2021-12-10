195117ff883213faec983ced53a39b0a6ef44eb5
senyokd <senyodzantoh@gmail.com>
2021-12-10 14:04:25 +0000

    added quiz app

---
+++ questions.js
@@ -0,0 +1,58 @@
+var questions = [{
+	"question":" Who is best known for using the phrase 'Fellow Ghanaians'? ",
+	"option1":" Swit Sixteen ",
+	"option2":" Efia Odo ",
+	"option3":" Seyram ",
+	"option4":" Nana Akuffo Addo ",
+	"answer": "4 "
+}, {
+	"question":" Who is the leader of The Seven Deadly Sins?  ",
+	"option1":" Hang ",
+	"option2":" Merlin ",
+	"option3":" Meliodas ",
+	"option4":" Elizabeth ",
+	"answer": "3"
+}, {
+	"question":" What is Senyo's middle name? ",
+	"option1":" Kwesi ",
+	"option2":" Kwadwo ",
+	"option3":" Kodjo  ",
+	"option4":" Kweku ",
+	"answer": "3"
+}, {
+	"question":" What is Ghana's official currency? ",
+	"option1":" Pounds ",
+	"option2":" Ghana Cedi ",
+	"option3":" Dollars ",
+	"option4":" Yen ",
+	"answer": "2 "
+}, {
+	"question":" Who developed this site?  ",
+	"option1":" Euda Haizel ",
+	"option2":" Etornam ",
+	"option3":" Senyo Kodjo Dzantor  ",
+	"option4":" Abena Asante  ",
+	"answer": " 3 "
+}, {
+	"question":" Among the following who is the best Pop Star? ",
+	"option1":" Drake ",
+	"option2":" Justin Bieber  ",
+	"option3":" Ariana Grande ",
+	"option4":" BTS ",
+	"answer": "2 "
+}, {
+	"question":" Which is the greatest football club?  ",
+	"option1":" Barcelona ",
+	"option2":" Liverpool ",
+	"option3":" Manchester United ",
+	"option4":" Real Madrid ",
+	"answer": " 3 "
+}, {
+	"question":" Which account is the most followed account on Instagram?  ",
+	"option1":" kwesiarthur_ ",
+	"option2":" foodporn ",
+	"option3":" christianoronaldo ",
+	"option4":" instagram ",
+	"answer": "3 "
+}
+]
